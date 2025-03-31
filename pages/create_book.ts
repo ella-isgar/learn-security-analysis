b8da5c8c5d542ee9c26259baf63f2fc846b773f5
ella-isgar <ellar.isgar@gmail.com>
2025-03-31 13:21:38 -0400

    RESOLVED xxs through exception vuln

--- pages/create_book.ts
+++ pages/create_book.ts
@@ -3,6 +3,10 @@ import Book from "../models/book";
 import express from "express";
 import bodyParser from "body-parser";
 import { appRateLimiter } from "../sanitizers/rateLimiter";
+import {
+  validateBookDetailsMiddleware,
+  RequestWithSanitizedBookDetails,
+} from "../sanitizers/bookSanitizer";
 
 const router = express.Router();
 
@@ -19,24 +23,32 @@ router.use(express.json());
  * @returns a newly created book for an existing author and genre in the database
  * @returns 500 error if book creation failed
  */
-router.post("/", appRateLimiter, async (req: Request, res: Response) => {
-  const { familyName, firstName, genreName, bookTitle } = req.body;
-  if (familyName && firstName && genreName && bookTitle) {
-    try {
-      const book = new Book({});
-      const savedBook = await book.saveBookOfExistingAuthorAndGenre(
-        familyName,
-        firstName,
-        genreName,
-        bookTitle
-      );
-      res.status(200).send(savedBook);
-    } catch (err: unknown) {
-      res.status(500).send("Error creating book: " + (err as Error).message);
+// router.post("/", appRateLimiter, async (req: Request, res: Response) => {
+router.post(
+  "/",
+  validateBookDetailsMiddleware,
+  async (req: RequestWithSanitizedBookDetails, res: Response) => {
+    const familyName = req.familyName;
+    const firstName = req.firstName;
+    const genreName = req.genreName;
+    const bookTitle = req.bookTitle;
+    if (familyName && firstName && genreName && bookTitle) {
+      try {
+        const book = new Book({});
+        const savedBook = await book.saveBookOfExistingAuthorAndGenre(
+          familyName,
+          firstName,
+          genreName,
+          bookTitle
+        );
+        res.status(200).send(savedBook);
+      } catch (err: unknown) {
+        res.status(500).send("Error creating book: " + (err as Error).message);
+      }
+    } else {
+      res.send("Invalid Inputs");
     }
-  } else {
-    res.send("Invalid Inputs");
   }
-});
+);
 
 export default router;
