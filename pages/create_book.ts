040ebeb37047fdafbc6a90445e63cf9a737dea64
ella-isgar <ellar.isgar@gmail.com>
2025-03-31 12:57:56 -0400

    RESOLVED rate limiting vulnerability

--- pages/create_book.ts
+++ pages/create_book.ts
@@ -1,7 +1,8 @@
-import { Request, Response } from 'express';
-import Book from '../models/book';
-import express from 'express';
-import bodyParser from 'body-parser';
+import { Request, Response } from "express";
+import Book from "../models/book";
+import express from "express";
+import bodyParser from "body-parser";
+import { appRateLimiter } from "../sanitizers/rateLimiter";
 
 const router = express.Router();
 
@@ -18,18 +19,23 @@ router.use(express.json());
  * @returns a newly created book for an existing author and genre in the database
  * @returns 500 error if book creation failed
  */
-router.post('/', async (req: Request, res: Response) => {
+router.post("/", appRateLimiter, async (req: Request, res: Response) => {
   const { familyName, firstName, genreName, bookTitle } = req.body;
   if (familyName && firstName && genreName && bookTitle) {
     try {
       const book = new Book({});
-      const savedBook = await book.saveBookOfExistingAuthorAndGenre(familyName, firstName, genreName, bookTitle);
+      const savedBook = await book.saveBookOfExistingAuthorAndGenre(
+        familyName,
+        firstName,
+        genreName,
+        bookTitle
+      );
       res.status(200).send(savedBook);
     } catch (err: unknown) {
-      res.status(500).send('Error creating book: ' + (err as Error).message);
+      res.status(500).send("Error creating book: " + (err as Error).message);
     }
   } else {
-    res.send('Invalid Inputs');
+    res.send("Invalid Inputs");
   }
 });
 
